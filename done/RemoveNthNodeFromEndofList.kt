a28f58ab44c766153185a98d7b70f8ad0cbdeabb
zhazgul <zhazgul@mustard.love>
2022-03-29 14:20:23 +0600

    29.03

---
+++ done/RemoveNthNodeFromEndofList.kt
@@ -0,0 +1,41 @@
+fun main() {
+    var node1 = ListNode(1)
+    val node2 = ListNode(2)
+//    val node3 = ListNode(3)
+//    val node4 = ListNode(4)
+//    val node5 = ListNode(5)
+    node1.next = node2
+//    node2.next = node3
+//    node3.next = node4
+//    node4.next = node5
+
+    printListNode(node1)
+    node1 = removeNthFromEnd(node1, 1)!!
+    printListNode(node1)
+}
+
+fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+    if (head == null) return null
+    var size = 0
+    var node = head
+    while (node != null) {
+        size++
+        node = node.next
+    }
+
+    if (n == size) return head.next
+
+    var counter = 0
+    node = head
+    var prev: ListNode = node
+    while (node != null) {
+        if (counter == size - n - 1)
+            prev = node
+        if (counter == size - n)
+            prev.next = node.next
+        println("${node.`val`}\t$counter\t${prev.`val`}")
+        counter++
+        node = node.next
+    }
+    return head
+}
