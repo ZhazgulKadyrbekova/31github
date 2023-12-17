a871973d04d0cf33fd891ba4cac716e2cc6f406f
zhazgul <zhazgul004@gmail.com>
2023-12-18 00:10:35 +0600

    leetcode problem

---
+++ done/ContainerWithMostWater.java
@@ -0,0 +1,25 @@
+package done;
+
+class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int start = 0, end = height.length - 1;
+        int area = 0, max = 0;
+        while (start < end) {
+            if (height[start] < height[end]) {
+                area = getArea(end - start, height[start]);
+                start++;
+            } else {
+                area = getArea(end - start, height[end]);
+                end--;
+            }
+            if (max < area) {
+                max = area;
+            }
+        }
+        return max;
+    }
+
+    private int getArea(int bottom, int side) {
+        return bottom * side;
+    }
+}
