a9425ab935abef2f71901505196f98a8a6e47f0c
zhazgul <zhazgul004@gmail.com>
2023-12-19 21:45:52 +0300

    template problem

---
+++ Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static void main(String[] args) {
+        var solution = new StringCompression();
+
+        char[] chars = new char[]{'a','a','b','b','c','c','c'};
+        System.out.println(solution.compress(chars));
+    }
+
+}
