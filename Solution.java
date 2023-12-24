9e6c442320f9b95e0c6768ca37368b4249e9e597
zhazgul <zhazgul004@gmail.com>
2023-12-24 21:08:26 +0300

    leetcode template problem

--- Solution.java
+++ Solution.java
@@ -2,9 +2,9 @@ public class Solution {
     public static void main(String[] args) {
         var solution = new PathCrossing();
 
-//        System.out.println(solution.isPathCrossing("NES"));
-//        System.out.println(solution.isPathCrossing("ENES"));
-//        System.out.println(solution.isPathCrossing("NESWW"));
+        System.out.println(solution.isPathCrossing("NES"));
+        System.out.println(solution.isPathCrossing("ENES"));
+        System.out.println(solution.isPathCrossing("NESWW"));
         System.out.println(solution.isPathCrossing("S"));
 
     }
