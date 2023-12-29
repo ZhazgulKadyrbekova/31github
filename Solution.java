66ddafcf706b893098bfe6315b6cd4163f2d6669
zhazgul <zhazgul004@gmail.com>
2023-12-29 13:44:14 +0300

    leetcode template problem

--- Solution.java
+++ Solution.java
@@ -1,12 +1,10 @@
+import done.MaximumNumberofVowelsinaSubstringofGivenLength;
+
 public class Solution {
     public static void main(String[] args) {
-        var solution = new PathCrossing();
+        var solution = new MaximumNumberofVowelsinaSubstringofGivenLength();
 
-        System.out.println(solution.isPathCrossing("NES"));
-        System.out.println(solution.isPathCrossing("ENES"));
-        System.out.println(solution.isPathCrossing("NESWW"));
-        System.out.println(solution.isPathCrossing("S"));
-
+        System.out.println(solution.maxVowels("abciiidef", 3));
     }
 
 }
