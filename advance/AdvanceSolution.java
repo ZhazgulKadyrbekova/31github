77f9cf4e17d7127fe66b83921fa89e5c2912dfd1
zhazgul <zhazgul004@gmail.com>
2024-01-03 23:44:41 +0600

    unfinished advance problem

---
+++ advance/AdvanceSolution.java
@@ -0,0 +1,14 @@
+package advance;
+
+import java.io.IOException;
+
+public class AdvanceSolution {
+
+    public static void main(String[] args) throws IOException {
+        Day15 solution = new Day15();
+
+        var res = solution.solve();
+        System.out.println(res);
+    }
+
+}
