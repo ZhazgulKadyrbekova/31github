077ed9415b9b90dbe5137e05258b345e7899fb5b
zhazgul <zhazgul004@gmail.com>
2024-04-28 14:25:43 +0200

    leetcode problem

---
+++ AsteroidCollision.java
@@ -0,0 +1,46 @@
+import java.util.Stack;
+
+public class AsteroidCollision {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int asteroid : asteroids) {
+            while (!stack.isEmpty() && asteroid < 0 && stack.lastElement() > 0) {
+                int dif = asteroid + stack.lastElement();
+                if (dif > 0) {
+                    asteroid = 0;
+                }
+                else if (dif < 0) {
+                    stack.pop();
+                }
+                else {
+                    stack.pop();
+                    asteroid = 0;
+                }
+            }
+            if (asteroid != 0) {
+                stack.push(asteroid);
+            }
+        }
+
+        return stack.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public int[] asteroidCollision2(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int asteroid : asteroids) {
+            while (!stack.isEmpty() && stack.lastElement() > 0 && stack.lastElement() < -asteroid) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty() || asteroid > 0 || stack.lastElement() < 0) {
+                stack.push(asteroid);
+            }
+            else if (asteroid < 0 && stack.lastElement() == -asteroid) {
+                stack.pop();
+            }
+        }
+
+        return stack.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+}
--- Solution.java
+++ Solution.java
@@ -1,17 +1,12 @@
-import done.BestTimeToBuyAndSellStockII;
-
 public class Solution {
     public static void main(String[] args) {
-        var solution = new BestTimeToBuyAndSellStockII();
+        var solution = new AsteroidCollision();
 
-        System.out.println(solution.maxProfit(new int[] {2,2,5}));
-        System.out.println(solution.maxProfit(new int[] {7,1,5,3,2,6,4}));
-        System.out.println(solution.maxProfit(new int[] {7,1,3,5,2,6,4}));
-        System.out.println(solution.maxProfit(new int[] {7,1,3,5,6,4}));
-        System.out.println(solution.maxProfit(new int[] {7,1,5,6,4}));
-        System.out.println(solution.maxProfit(new int[] {7,1,5,3,6,4,0}));
-        System.out.println(solution.maxProfit(new int[] {7,1,5,3,6,4,0,6}));
-        System.out.println(solution.maxProfit(new int[] {7,6,4,3,1}));
+//        int[] res = solution.asteroidCollision(new int[] {10,2,-5});
+//        int[] res = solution.asteroidCollision(new int[] {10,2,-5,5,10,-5});
+        int[] res = solution.asteroidCollision(new int[] {-10,2,-5,5,10,-5});
+        for (int i : res) {
+            System.out.println(i);
+        }
     }
-
 }
