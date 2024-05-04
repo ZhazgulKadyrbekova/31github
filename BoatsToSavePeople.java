2328b99da468047e19c51e13cd3e4c751ad83df2
zhazgul <zhazgul004@gmail.com>
2024-05-04 13:35:12 +0200

    leetcode problem

---
+++ BoatsToSavePeople.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class BoatsToSavePeople {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+
+        int start = 0, end = people.length - 1, count = 0;
+        while (start <= end) {
+            if (people[start] + people[end] <= limit) {
+                start++;
+            }
+            end--;
+            count++;
+        }
+
+        return count;
+    }
+}
--- Solution.java
+++ Solution.java
@@ -1,9 +1,9 @@
-import done.ReversePrefixOfWord;
-
 public class Solution {
     public static void main(String[] args) {
-        var solution = new ReversePrefixOfWord();
+        var solution = new BoatsToSavePeople();
 
-        System.out.println(solution.reversePrefix("abcdefd", 'd'));
+        System.out.println(solution.numRescueBoats(new int[] {1,2},3));
+        System.out.println(solution.numRescueBoats(new int[] {3,2,2,1},3));
+        System.out.println(solution.numRescueBoats(new int[] {3,5,3,4},5));
     }
 }
