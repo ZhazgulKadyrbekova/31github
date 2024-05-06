1c2627738476a184df07eb513a8ce7df59148458
zhazgul <zhazgul004@gmail.com>
2024-05-06 21:37:12 +0200

    leetcode problem

--- Solution.java
+++ Solution.java
@@ -1,22 +1,24 @@
-import done.DeleteNodeinaLinkedList;
+import done.RemoveNodesFromLinkedList;
 
 public class Solution {
     public static void main(String[] args) {
-        var solution = new DeleteNodeinaLinkedList();
+        var solution = new RemoveNodesFromLinkedList();
 
-        ListNode node1 = new ListNode(4);
-        ListNode node2 = new ListNode(5);
-        ListNode node3 = new ListNode(1);
-        ListNode node4 = new ListNode(9);
-        node1.setNext(node2);
-        node2.setNext(node3);
-        node3.setNext(node4);
+        ListNode node1 = new ListNode(5);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(13);
+        ListNode node4 = new ListNode(3);
+        ListNode node5 = new ListNode(8);
+        node1.next = node2;
+        node2.next = (node3);
+        node3.next = (node4);
+        node4.next = (node5);
 
-        solution.deleteNode(node1);
+        ListNode newNode = solution.removeNodes(node1);
 
-        while (node1 != null) {
-            System.out.println(node1.getVal());
-            node1 = node1.getNext();
+        while (newNode != null) {
+            System.out.println(newNode.val);
+            newNode = newNode.next;
         }
     }
 }
---
+++ done/RemoveNodesFromLinkedList.java
@@ -0,0 +1,37 @@
+package done;
+
+import java.util.Stack;
+
+public class RemoveNodesFromLinkedList {
+    public ListNode removeNodes(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+
+        while (head != null) {
+            if (stack.isEmpty()) {
+                stack.push(head.val);
+                head = head.next;
+                continue;
+            }
+
+            int prev = stack.lastElement();
+            while (!stack.isEmpty() && prev < head.val) {
+                stack.pop();
+                prev = stack.isEmpty() ? 0 : stack.lastElement();
+            }
+            if (prev == 0 || prev >= head.val) {
+                stack.push(head.val);
+            }
+            head = head.next;
+        }
+//        System.out.println(stack);
+
+        ListNode newNode = null;
+        while (!stack.isEmpty()) {
+            int val = stack.pop();
+            ListNode node = new ListNode(val, newNode);
+            newNode = node;
+        }
+
+        return newNode;
+    }
+}
