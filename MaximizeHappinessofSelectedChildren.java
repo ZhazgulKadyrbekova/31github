6cddd597c82fc67c45281d35458fccbc3257285d
zhazgul <zhazgul004@gmail.com>
2024-05-09 12:43:41 +0200

    leetcode daily problem

---
+++ MaximizeHappinessofSelectedChildren.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class MaximizeHappinessofSelectedChildren {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+        int len = happiness.length - 1;
+        long sum = 0;
+        for (int i = 0; i < k; i++) {
+            int val = (happiness[len - i] - i) > 0 ? (happiness[len - i] - i) : 0;
+            sum += val;
+        }
+
+        return sum;
+    }
+}
